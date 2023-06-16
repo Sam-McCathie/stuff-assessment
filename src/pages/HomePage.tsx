4fa5aa68e89b139835af0adedcc9eb24100be331
Sam McCathie <sammccathie2@gmail.com>
2023-06-17 11:21:59 +1200

    Fetch data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
   const navigate = useNavigate();
+
+  const [articleData, setArticleData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    const fetchArticlesData = async () => {
+      try {
+        const response = await fetch(
+          `https://www.stuff.co.nz/static/spade/nCuL9ZmbMXzhGbHTJNJYU6i45y9hj0DJrhPteuU6MGB68zM5goWqk5Q1aNkh.json`
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch article data");
+        }
+        const data = await response.json();
+        setArticleData(data.stories);
+      } catch (error: any) {
+        setErrorMessage(error.message);
+      }
+    };
+
+    fetchArticlesData();
+  }, []);
+
+  if (!articleData) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (errorMessage) {
+    return <h1>{errorMessage}</h1>;
+  }
+
+  console.log(articleData);
 
   const handleArticleClick = (id: number, title: string) => {
     navigate(`/articles/${id}`, { state: { title } });
